76e5619dc95960069ee9adee80f0b53cd666ea13
Berta Balazs <berta.balazs@gmail.com>
2022-03-03 17:16:58 +0100

    events

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function init(mathFunction) {
+/* function init(mathFunction) {
     var a = 5;
     var b = 6;
 
@@ -8,25 +8,25 @@ function init(mathFunction) {
     let c = mathFunction(b,a)
         
         
-    /*    (function(){
+        (function(){
             var c = b - a;    
             console.log(c); 
-        }) ();         */
+        }) ();         
     } else {
         let c = mathFunction(a,b)
-   /*      (function(){
+        (function(){
             var c = a - b;
             console.log(c); 
         })();
-         */
+        
         console.log(c);
     };
-
+ */
 
   /*   console.log(c); */
-}
 
-const initC = (firstNumber, secondNumber) => {
+
+/* const initC = (firstNumber, secondNumber) => {
     return firstNumber - secondNumber;
 }
 
@@ -34,4 +34,58 @@ const initD = (firstNumber, secondNumber => {
     return firstNumber * secondNumber;
 })
 
-init(initD);
+init(initD); */
+
+//form kesztiese, 3 input mezovel , kis css mehet bele (1 gomb legyen benne,) az input mezokre mindegyikre input esemenyt tegyunk ra
+//loggolja ki az értékét az input mezőnek
+//50-kor tali
+
+const formHTML = () => {
+    return `
+    <form >
+            <input id="input1" name="input1" type="text">
+            <input id="input2" name="input2" type="text">
+            <input id="input3" name="input3" type="text">
+            <select name="animals" id="animals">
+                <option value="cats">cats</option>
+                <option value="dogs">dogs</option>
+                <option value="both">both</option>
+            </select>
+            <button>Send</button>
+    </form>
+    `
+}
+
+function loadEvent() {
+    console.log("load");
+    const rootElement = document.getElementById("root")
+    rootElement.insertAdjacentHTML("beforeend", formHTML())
+
+    const form = rootElement.querySelector("form")
+
+    const inputList = document.querySelectorAll('input')
+    console.log(inputList);
+    /* for (const input of inputList) {
+        input.addEventListener("input", function(event){
+            console.log(event.target);
+        })
+    } */
+    Array.from(inputList).map(function(input){
+        input.addEventListener("input", function(event){
+            console.log(event.target);
+        })
+    })
+
+    form.querySelector("select").addEventListener("input", function(){
+        console.log(event.target.value);
+    })
+
+    form.addEventListener("sumbit", function(event){
+        event.preventDefault()
+        console.log(event.target);
+    })
+
+
+}
+
+window.addEventListener("load", loadEvent)
